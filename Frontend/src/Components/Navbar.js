4e91785c8ed22663c00b15b88dd8cdd32e76a7e2
Jenesh <jenesh.napit@gmail.com>
2020-02-06 16:32:46 -0500

    Created and imported the Navbar. Initilized state in App component.

--- Frontend/src/Components/Navbar.js
+++ Frontend/src/Components/Navbar.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({ logoutUser, isUserLoggedIn }) => {
+    const LoggedInRoutes = () => (
+        <>
+            <nav>
+                <Link to='/'>Home</Link>
+                <Link to='/users'>Users</Link>
+                <Link to='/profile'>Profile</Link>
+            </nav>
+            <button className='btn btn-logout' onClick={logoutUser}>Logout</button>
+        </>
+    )
+    const LoggedOutRoutes = () => (
+        <nav>
+            <Link to='/'>Home</Link>
+            <Link to='/login'>Login</Link>
+            <Link to='/signup'>Signup</Link>
+        </nav>
+    )
     return (
         <div>
-            Navbar
+            {isUserLoggedIn ? LoggedInRoutes() : LoggedOutRoutes()}
         </div>
     )
 }
